54bc1df7843e252b2ebe42657bd50b744a72d3b7
Theau Pauwels <83910999+theau-pauwels@users.noreply.github.com>
2026-09-08 09:01:33 +0200

    fix: configure Astro for Vercel SSR

--- webapp/astro.config.mjs
+++ webapp/astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   integrations: [react(), tailwind()],
   vite: {
     resolve: {
